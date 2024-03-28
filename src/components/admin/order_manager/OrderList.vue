f86e52b0f9f5e9db0f5d6f5579c29aee1dab9b33
HuynhNhutHoa <hoab2005755@student.ctu.deu.vn>
2024-03-28 16:53:51 +0700

    Project_Backend_Sua_Doi_Lan_2

--- src/components/admin/order_manager/OrderList.vue
+++ src/components/admin/order_manager/OrderList.vue
@@ -1,44 +1,46 @@
 <template>
-    <div class="table-responsive">
-        <table class="table table-striped table-bordered">
-            <thead class="thead-dark text-center ">
-                <tr>
-                    <th scope="col">Người mượn</th>
-                    <th scope="col">Ngày mượn</th>
-                    <th scope="col">Ngày trả</th>
-                    <template v-for="book in contacts[0].books" :key="book.bookId">
-                        <th scope="col">Sách</th>
-                        <th scope="col">Số lượng</th>
-                    </template>
-                    <th scope="col">Trạng thái</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(contact, index) in contacts" :key="contact._id" :class="{ active: index === activeIndex }"
-                    @click="updateActiveIndex(index)">
-                    <td>{{ contact.name }}</td>
-                    <td>{{ contact.ngayMuon }}</td>
-                    <td>{{ contact.ngayTra }}</td>
-                    <template v-for="book in contact.books" :key="book.bookId">
-                        <td>{{ book.title }}</td>
-                        <td>{{ book.quantity }}</td>
-                    </template>
-                    <td>{{ contact.status }}</td>
-                    <td>
-                        <div class="btn-group" role="group">
-                            <router-link :to="{ name: 'order.edit', params: { id: contact._id }, }" class="nav-link">
-                                <span class="btn btn-sm btn-success" style="white-space: nowrap;">
-                                    <i class="fas fa-plus"></i> Hiệu chỉnh
-                                </span>
-                            </router-link>
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+    <div>
+        <div v-for="(contact) in contacts" :key="contact._id" class="list-group mt-5">
+            <div class="d-flex w-100 justify-content-between list-group-item">
+                <h6>{{ contact.ngayMuon }} -> {{ contact.ngayTra }}</h6>
+                <h5 class="mb-1">{{ contact.name }}</h5>
+                <p class="mb-1">
+                <h6> Trạng thái:
+                    <span :class="{
+            'text-warning': contact.status === 'Đang đợi duyệt',
+            'text-info': contact.status === 'Đang mượn',
+            'text-primary': contact.status === 'Đã trả',
+            'text-danger': contact.status === 'Đã hủy' || contact.status === 'Quá hạn trả'
+        }">
+                        {{ contact.status }}
+                    </span>
+                </h6>
+                </p>
+            </div>
+
+            <div class="list-group-item">
+                <div class="row">
+                    <div class="col-10">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item" v-for="(book, bookIndex) in contact.books" :key="book.bookId">
+                                <strong>{{ 'Sách ' + (bookIndex + 1) }}:</strong>
+                                {{ book.title }} - Số lượng: {{ book.quantity }}
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-2 d-flex justify-content-end align-items-center">
+                        <router-link :to="{ name: 'order.edit', params: { id: contact._id }, }">
+                            <button class="btn btn-sm btn-success" style="white-space: nowrap;">
+                                <i class="fas fa-plus"></i> Hiệu chỉnh
+                            </button>
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
 
 <script>
 export default {
@@ -55,22 +57,3 @@ export default {
 
 };
 </script>
-
-<style scoped>
-.table {
-    width: 100%;
-}
-
-.table th,
-.table td {
-    vertical-align: middle;
-}
-
-.table tbody tr:hover {
-    background-color: #f2f2f2;
-}
-
-.active {
-    background-color: #d3d3d3;
-}
-</style>
